70eb0cadc3fcf5f084ccd602d37292c4454ef522
YusufAkkurt <ysfakkurt@hotmail.com>
2021-03-14 15:04:47 +0300

    Proje aktarıldı

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Product } from "src/app/models/product";
+import { ProductService } from "../../services/product.service";
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+
+export class ProductComponent implements OnInit {
+
+  products: Product[] = []
+  dataLoaded = false
+
+  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      if(params["categoryId"])
+        return this.getProductsByCategory(params["categoryId"])
+        
+      return this.getProducts()
+    });
+  }
+
+  getProducts() {
+    this.productService.getProducts().subscribe((response) => {
+      this.products = response.data
+      this.dataLoaded = true
+    });
+  }
+
+  getProductsByCategory(categoryId: number) {
+    this.productService.getProductsByCategory(categoryId).subscribe((response) => {
+      this.products = response.data
+      this.dataLoaded = true
+    });
+  }
+}
